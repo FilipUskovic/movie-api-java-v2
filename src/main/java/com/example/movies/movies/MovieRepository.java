5b7ba89439f88d7bf93620fe24a190547401ed14
Filip <44414201+FilipUskovic@users.noreply.github.com>
2023-08-05 14:39:57 +0200

    Add files via upload

---
+++ src/main/java/com/example/movies/movies/MovieRepository.java
@@ -0,0 +1,11 @@
+package com.example.movies.movies;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import org.bson.types.ObjectId;
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+    Optional<Movie> findMovieByImdbId(String imdbId);
+}
